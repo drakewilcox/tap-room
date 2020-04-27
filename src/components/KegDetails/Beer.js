ea51cacd01338efadfdda25c460eeee86873a634
Drake Wilcox <drakewilcox@gmail.com>
2020-04-27 12:47:41 -0700

    add a salesReport form feature.

--- src/components/KegDetails/Beer.js
+++ src/components/KegDetails/Beer.js
@@ -2,7 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Beer = (props) => {
+
+  function handleSalesReport(event) {
+    event.preventDefault();
+    let newLevel = props.beer.kegLevel - event.target.pintsSold.value;
+    if(props.beer.kegLevel === "Sold Out") {
+      newLevel = "Sold Out";
+    }
+    else if(newLevel <= 0) {
+      newLevel = "Sold Out";
+    }
+    props.onNewSalesReport({
+      pintsSold: newLevel,
+      id: props.beer.id, 
+      
+   
+    })
+  }
+ 
   return (
+  <React.Fragment>
+
     <div>
       <h3>Beer Details:</h3>
       <p><em>Click on a menu item to view details and make sales reports: </em></p>
@@ -14,11 +34,21 @@ const Beer = (props) => {
       <p><strong>Pint Price:</strong> {props.beer.pintPrice}</p>
       <p><strong>KegLevel:</strong> {props.beer.kegLevel}</p>
     </div>
+    <form onSubmit={ handleSalesReport }>
+      <input
+        type='text'
+        name='pintsSold'
+        placeholder='Pints Sold'/>
+      <button type='submit'>Report</button>
+    </form>
+  </React.Fragment>
+    
   )
 }
 
 Beer.propTypes = {
   beer: PropTypes.object,
+  onNewSalesReport: PropTypes.func
 }
 
 export default Beer; 
